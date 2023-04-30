0e339ad0f9fdb6cbf3f3cb31ab3faf3111b4fd29
Vivian Xu <hi@vivianxu.com>
2023-04-30 11:47:02 +1000

    💄 Add temporary logo

---
+++ app/Layout/components/Header/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import {FC} from 'react'
+
+const Logo: FC = () => (
+  <div className="font-display text-cobalt leading-none">
+    <span className="font-bold text-4xl tracking-tight">DPL</span>{' '}
+    <span className="text-2xl uppercase tracking-wide">Group</span>
+  </div>
+)
+
+export default Logo
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      cobalt: {
+        50: '#F2F5FB',
+        100: '#E5EBF7',
+        200: '#BFCEEA',
+        300: '#99B0DE',
+        400: '#7393D1',
+        500: '#4D75C5',
+        600: '#2658B8',
+        700: '#003AAC',
+        800: '#003292',
+        900: '#002978',
+        950: '#00205F',
+        DEFAULT: '#003AAC',
+      },
+    },
+    fontFamily: {
+      display: ['var(--open-sans-font)'],
+    },
     extend: {},
   },
   plugins: [],
 }
-
